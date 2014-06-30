ce15b3a42c572a9608c61b5d78b8ab3dbccdb785
ajaxj <ajaxj@qq.com>
2014-06-30 18:06:13 +0800

    init

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from backend.models import Category
+
 
 def index(request):
-    return render_to_response("app/index.html")
+    _categories = Category.objects.all()
+    print _categories
+    return render_to_response("app/index.html",{'categories':_categories})
