e481471582432e50d440da6f33abaff2e24bd3d0
Administrator <ajaxj@ubuntu.ubuntu-domain>
2014-08-18 14:52:13 +0800

    add new

--- backend/views.py
+++ backend/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, render_to_response
-from backend.models import Category
+from backend.models import Category,Movie
 
 
 def index(request):
     return render_to_response("backend/index.html")
 
 
-
+# category all list
 def categories(request):
     category = Category()
     category.name = "name1"
@@ -14,3 +14,23 @@ def categories(request):
     category.save()
     _categories = Category.objects.all()
     return render_to_response("backend/categories.html")
+
+
+# movie all list
+def movies(request):
+	movie = Movie()
+	movie.title = "title1"
+	movie.save()
+
+
+
+# init database 
+def initdb(request):
+	category = Category()
+	category.name = "name1"
+	category.cname = "cname1"
+	category.save()
+	movie = Movie()
+	movie.title = "title1"
+	movie.save()
+	return 
